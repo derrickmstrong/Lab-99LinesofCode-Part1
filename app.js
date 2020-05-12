4fba8109d1d8d58cd24ddb26249e0e3e624c18a3
Derrick Strong <derrickmstrong@gmail.com>
2020-05-12 08:37:29 -0500

    Clean version

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 let friends = ['Sydni', 'Hafeeza', 'Charles', 'Josh', 'Ana'];
 
 for (let i = 0; i < friends.length; i++) {
-  const name = friends[i];
-  console.log(`${name.toUpperCase()}:`);
+  const friend = friends[i];
+  console.log(`${friend.toUpperCase()}:`);
 
   function linesOfCodeSong(
     count = 99,
@@ -12,24 +12,24 @@ for (let i = 0; i < friends.length; i++) {
     chorus = 'code in the file'
   ) {
     for (let i = count; i > 0; i--) {
-      console.log(
-        `${count} ${refrain}s of ${chorus}, ${count} ${refrain}s of ${refrain2}; ${name} ${prechorus}, ${
-          count - 1
-        } ${refrain}s of ${chorus}`
-      );
-      count--;
-      if (count == 2) {
+      if (count === 2) {
         console.log(
-          `${count} ${refrain}s of ${chorus}, ${count} ${refrain}s of ${refrain2}; ${name} ${prechorus}, ${
+          `${count} ${refrain}s of ${chorus}, ${count} ${refrain}s of ${refrain2}; ${friend} ${prechorus}, ${
             count - 1
           } ${refrain} of ${chorus}`
         );
+        count--;
+      } else if (count === 1) {
         console.log(
-          `${count - 1} ${refrain} of ${chorus}, ${
-            count - 1
-          } ${refrain} of ${refrain2}; ${name} ${prechorus}, no more ${refrain}s of ${chorus}`
+          `${count} ${refrain} of ${chorus}, ${count} ${refrain} of ${refrain2}; ${friend} ${prechorus}, no more ${refrain}s of ${chorus} \n`
         );
-        break;
+      } else {
+        console.log(
+          `${count} ${refrain}s of ${chorus}, ${count} ${refrain}s of ${refrain2}; ${friend} ${prechorus}, ${
+            count - 1
+          } ${refrain}s of ${chorus}`
+        );
+        count--;
       }
     }
   }
